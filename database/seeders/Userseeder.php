20c5bbd29d62f490cefa40c7a41bed927606495f
shugo1125 <iamsyu25@icloud.com>
2024-12-14 18:13:28 +0900

    no message

--- database/seeders/Userseeder.php
+++ database/seeders/Userseeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
-class Userseeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
